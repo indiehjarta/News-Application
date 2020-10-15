daae91436eed91e71177a2627e8789644e646028
Carolina Arvidsson <carolinaarvidsson_@live.se>
2020-10-15 14:07:18 +0200

    still worked on css to tagbar

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,8 +3,8 @@ import React, { Component, Fragment } from 'react';
 // imports components
 import Header from '../comps/header/Header';
 import TagsBar from '../comps/TagsBar';
-/*import NewsCarousel from '../comps/NewsCarousel';
-import NewsCard from '../comps/NewsCard';*/
+//import NewsCarousel from '../comps/NewsCarousel';
+//import NewsCard from '../comps/NewsCard';
 
 class HomeScreen extends Component {
     render () {
